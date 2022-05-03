0123a7cffdac49ccd7983a9e84373e450637c99a
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-05-03 23:16:28 +0800

    script/build: add decoder target

--- scripts/build.js
+++ scripts/build.js
@@ -1,19 +1,24 @@
 import { writeFileSync } from 'fs'
+import { resolve } from 'path'
 import { PROJECT_ROOT } from 'lib/env.js'
 import Decoder from 'lib/decoder.js'
-import { resolve } from 'path'
+import Parser from 'lib/parser.js'
+import { ensureDirSync } from 'fs-extra'
 // Get target from cli parameter
 const [target = 'RV64G'] = process.argv.slice(2)
 // Collect all related data
 const raw = (await import(resolve(PROJECT_ROOT, 'targets', `${target}.js`))).default
+// Secure build path
+const buildPath = resolve(PROJECT_ROOT, 'build', target)
+ensureDirSync(buildPath)
 // Save raw.json
 writeFileSync(
-	resolve(PROJECT_ROOT, 'build', 'raw.json'),
+	resolve(buildPath, 'raw.json'),
 	JSON.stringify(raw, null, 4)
 )
 // Create decode strategy
 writeFileSync(
-	resolve(PROJECT_ROOT, 'build', 'decoder.js'),
+	resolve(buildPath, 'decoder.js'),
 	new Decoder(
 		Object.fromEntries(
 			Object
@@ -22,7 +27,23 @@ writeFileSync(
 		)
 	).js
 )
-//
-
+// Create parser library
+writeFileSync(
+	resolve(buildPath, 'parser.js'),
+	`export default {\n\t${
+		Object
+			.entries(raw)
+			.map(([id, { args }]) => `'${id}': code => ({\n\t\t${
+				Object
+					.entries(args)
+					.map(([name, range]) => `${/(^\d)|[.-]/gi.test(name) ? `'${name}'` : name}: ${new Parser(range).js}`)
+					.join(',\n\t\t')
+			}\n\t})`)
+			.join(',\n\t')
+	}\n}`.trim()
+)
 // Make docs
+let x = {
+	['hello'](a) { return a }
+}
 
