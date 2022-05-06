d9cd6bfc5694865bf59e1a9d21ec13b0727db303
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-05-06 23:30:23 +0800

    web: improve state-transfer view

--- web/src/components/instruction/stateTransfer.vue
+++ web/src/components/instruction/stateTransfer.vue
@@ -1,26 +1,40 @@
 <script setup>
+import argColor from './argColor';
 const props = defineProps({
 	exec: Object
 })
 </script>
 
 <template>
-	<container flex-column :pad="false" style="margin-bottom: 1em">
+	<container flex-column :pad="false">
 		<container
 			flex-row
 			content-center
 			v-for="(exp, target) in props.exec"
 			:key="target"
 			:pad="false"
+			style="margin: 0.5em 0; font-family: monospace, monospace"
 		>
-			<badge :name="target" style="min-width: 2em; text-align: center">{{
-				target
-			}}</badge>
+			<badge
+				:name="target"
+				:type="`solid ${argColor(target)}`"
+				style="
+					min-width: 3em;
+					text-align: center;
+					margin: 0;
+					font-size: 1em;
+				"
+				>{{ target }}</badge
+			>
 			<i
 				class="fa fa-arrow-left"
-				style="font-size: 0.8em; color: var(--ct-gray); margin: 0 0.5em"
+				style="
+					font-size: 0.8em;
+					color: var(--ct-gray);
+					margin: 0 1em;
+				"
 			></i>
-			<pre>{{ exp }}</pre>
+			<span style="color: var(--ct-blue)">{{ exp }}</span>
 		</container>
 	</container>
 </template>
