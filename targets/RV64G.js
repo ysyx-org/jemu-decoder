dad5a5af361ae1b6ce3f9d4d0eba77b49eb5795e
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-05-06 19:37:43 +0800

    arch: format change, switch from js to yml

--- targets/RV64G.js
+++ targets/RV64G.js
@@ -1,15 +1,15 @@
-import { makeTarget } from 'lib/target.js'
+import make from 'lib/makeTarget.js'
 export default {
 	// riscv32
-	...await makeTarget('riscv32/A', { type: 'RV32A' }),
-	...await makeTarget('riscv32/D', { type: 'RV32D' }),
-	...await makeTarget('riscv32/F', { type: 'RV32F' }),
-	...await makeTarget('riscv32/I', { type: 'RV32I' }),
-	...await makeTarget('riscv32/M', { type: 'RV32M' }),
+	...await make('riscv32/A', { type: 'RV32A' }),
+	...await make('riscv32/D', { type: 'RV32D' }),
+	...await make('riscv32/F', { type: 'RV32F' }),
+	...await make('riscv32/I', { type: 'RV32I' }),
+	...await make('riscv32/M', { type: 'RV32M' }),
 	// riscv64
-	...await makeTarget('riscv64/A', { type: 'RV64A' }),
-	...await makeTarget('riscv64/D', { type: 'RV64D' }),
-	...await makeTarget('riscv64/F', { type: 'RV64F' }),
-	...await makeTarget('riscv64/I', { type: 'RV64I' }),
-	...await makeTarget('riscv64/M', { type: 'RV64M' }),
+	...await make('riscv64/A', { type: 'RV64A' }),
+	...await make('riscv64/D', { type: 'RV64D' }),
+	...await make('riscv64/F', { type: 'RV64F' }),
+	...await make('riscv64/I', { type: 'RV64I' }),
+	...await make('riscv64/M', { type: 'RV64M' }),
 }
