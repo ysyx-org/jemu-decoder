d4fbf2beea0e3c2db6283215bccb36dbf443c609
Yuxuan Zhang <yuxuan@yuxuanzhang.net>
2022-05-09 16:08:06 +0800

    web: fix 1 bit digit-range display (close #1)

--- web/src/components/instruction/digitRange.vue
+++ web/src/components/instruction/digitRange.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { parserOptions } from "@vue/compiler-dom";
 import { computed } from "@vue/runtime-core";
 import argColor from "./argColor";
 
@@ -10,7 +11,26 @@ const
 		pos: Array,
 		vec: Array,
 	}),
-	color = computed(() => argColor(props.name))
+	color = computed(() => argColor(props.name)),
+	rangeWidth = computed(() => props.pos
+		.slice(0, 2)
+		.reduce((a, b) => a > b ? a - b : b - a)
+	),
+	rangeText = computed(() => {
+		const [l, r = l] = props.vec
+		// Single digit variable
+		if (props.width <= 1 && l == 0 && r == 0) return ''
+		// Range segment of one digit
+		if (l == r) return `[${l}]`
+		// Normal range
+		return `[${l}:${r}]`
+	}),
+	hang = computed(() => {
+		const
+			textWidth = props.name.length + rangeText.value.length * 0.8,
+			elWidth = rangeWidth.value * 3
+		return textWidth > elWidth
+	})
 </script>
 
 <template>
@@ -30,16 +50,36 @@ const
 				border: `1px solid var(--cf-${color})`,
 				'background-color': `var(--cf-${color})`,
 			}"
+			:class="hang ? 'hang' : ''"
 		>
-			<b>{{ props.name }}</b>
-			<span
-				style="font-weight: 100; opacity: 0.8"
-				v-if="props.vec[0] !== props.vec[1]"
-				>[{{ props.vec.join(":") }}]</span
+			<div
+				range
+				style="font-weight: 100; opacity: 0.8 font-size: 0.8em;"
+				v-if="rangeText"
 			>
+				{{ rangeText }}
+			</div>
+			<div name>{{ props.name }}</div>
 		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
+[digit-range] {
+	[content] {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: row-reverse;
+		flex-wrap: wrap;
+		[range] {
+			opacity: 0.6;
+			font-weight: lighter;
+		}
+		&.hang [range] {
+			font-size: 0.8em;
+			margin: 0.125em 0 0.25em 0;
+		}
+	}
+}
 </style>
--- web/src/components/instruction/digitRuler.vue
+++ web/src/components/instruction/digitRuler.vue
@@ -7,7 +7,16 @@ const props = defineProps({
 </script>
 
 <template>
-	<div style="position: absolute; top: 0; left: 0; right: 0; bottom: 0">
+	<div
+		style="
+			position: absolute;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			opacity: 0.5;
+		"
+	>
 		<span v-for="pos in list" :key="pos" :style="{ '--pos': pos }">
 			<div rule></div>
 			<div
@@ -33,22 +42,32 @@ const props = defineProps({
 [rule-text] {
 	position: absolute;
 	--x: calc(var(--pos) * var(--unit));
+	--c: var(--ct-gray-dark);
 }
 
 [rule] {
 	top: 0;
 	bottom: 0;
+	width: 1px;
 	right: calc(var(--x) - 0.5px);
-	border-left: 1px dashed var(--cb-gray-dark);
+	background-image: linear-gradient(to bottom,
+		var(--c) 12%, transparent 12%,
+		transparent 20%, var(--c) 20%,
+		var(--c) 32%, transparent 32%,
+		transparent 40%, var(--c) 40%,
+		var(--c) 52%, transparent 52%,
+		transparent 60%, var(--c) 60%,
+		var(--c) 72%, transparent 72%
+	);
 }
 
 [rule-text] {
 	top: 0;
-	color: var(--ct-gray-light);
+	color: var(--c);
 	font-size: 0.7em;
 	font-weight: bold;
 	line-height: 100%;
-	padding: 0 calc(var(--unit) * 0.1);
+	padding: 0 calc(var(--unit) * 0.125);
 	&.left {
 		right: var(--x);
 	}
